d02ce76c8d964f7a4da859d7c599c04eb7eb4673
UP680879 <UP680879@myport.ac.uk>
2016-02-08 23:32:42 +0000

    db_connect.php
    Hello this is the file for database connection 
    this is the second time i upload but it just don't save in github?

---
+++ db_connect.php
@@ -0,0 +1,29 @@
+<?php	
+
+/*
+Here we are going to keep recored of the database connection_aborted
+like the use, host, server and password
+
+*/
+	$conn_error = 'Not connected'; // this is the message error 
+	
+	$db_name = "employees";  // this is the database name
+	
+	$un = 'root1';    // this is the username 
+	
+	$pw = 'root';   // this is the passowrd
+	
+	$host = 'localhost';  // this is the localhost server
+	
+	@mysql_connect($host, $un, $pw) or die(mysql_error($conn_error));
+
+	//Here we keep recored of the sql command for do the coonection to the database	
+
+	//echo('Connected to my SQL DataBase User ');
+	
+	@mysql_select_db($db_name) or die (mysql_error());
+	
+	//Here to select the specific database name with the sql command 
+	
+	//echo("<br/>Connected to DataBase ".$db_name);
+?>
